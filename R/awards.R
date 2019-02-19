6889338793fa333de293e067053d4a721f92ca65
Philip Bulsink <bulsinkp@gmail.com>
2019-02-19 15:28:41 -0500

    Awards

---
+++ R/awards.R
@@ -0,0 +1,19 @@
+#' Get Award
+#'
+#' @description Get the NHL awards, including historical awards. Or, get one single award
+#' @param award Optional, get specific award information. Notable awards include Stanley Cup = 1, Conn smythe = 7, Vezina = 18.
+#'
+#' @return The API output of awards
+#' @export
+getAward <- function(award = NULL) {
+  if(!is.null(award)) {
+    #checks to prevent bad API calls from progressing
+    stopifnot(length(award) == 1)
+    stopifnot(is.numeric(award))
+
+    query <- querybuilder('awards', award)
+  } else {
+    query <- 'awards'
+  }
+  return(getAPI(query = query))
+}
