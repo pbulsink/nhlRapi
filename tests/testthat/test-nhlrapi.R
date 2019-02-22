b916f6016ed245c5692de28f986cf388defd18e3
Philip Bulsink <bulsinkp@gmail.com>
2019-02-22 10:27:35 -0500

    making robust the regex check (cross platform) by removing © character validation

--- tests/testthat/test-nhlrapi.R
+++ tests/testthat/test-nhlrapi.R
@@ -3,5 +3,7 @@ context("test-nhlrapi")
 test_that("query queries", {
   q<-getAPI(query='teams')
   expect_type(q, 'list')
-  expect_match(q$copyright, 'NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 20[0-9][0-9]. All Rights Reserved.')
+  expect_type(q$copyright, 'character')
+  expect_equal(Encoding(q$copyright), 'UTF-8')
+  expect_match(q$copyright, 'NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams.')
 })
