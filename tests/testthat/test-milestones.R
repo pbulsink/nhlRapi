09c8719cf195020ba58ab268d91b1091f1284862
Philip Bulsink <bulsinkp@gmail.com>
2019-03-13 19:59:54 -0400

    buildignore git and fix milestones test (someone hit 1000 points exactly)

--- tests/testthat/test-milestones.R
+++ tests/testthat/test-milestones.R
@@ -1,9 +1,9 @@
 context("test-milestones")
 
-test_that("multiplication works", {
+test_that("milestones works", {
   q<-getMilestone('1000-point-career')
   expect_s3_class(q, 'nhl_api')
-  expect_true(all(q$data$careerPoints > 1000))
+  expect_true(all(q$data$careerPoints >= 1000))
 
   #Expected Failures
   expect_error(getMilestone(milestone = 'bad'))
